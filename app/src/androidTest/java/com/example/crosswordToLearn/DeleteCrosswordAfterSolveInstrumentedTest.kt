906a4c2bd74f76607b65ff1ac802879f0d18b023
greenEkatherine <sokolik-katya@mail.ru>
2021-01-24 21:48:27 +0100

    38: checking write access

--- app/src/androidTest/java/com/example/crosswordToLearn/DeleteCrosswordAfterSolveInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/crosswordToLearn/DeleteCrosswordAfterSolveInstrumentedTest.kt
@@ -1,15 +1,37 @@
 package com.example.crosswordToLearn
 
+import android.os.Environment
+import android.util.Log
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.File
+import java.io.FileOutputStream
 
 @RunWith(AndroidJUnit4::class)
 class DeleteCrosswordAfterSolveInstrumentedTest : SolveCrossword(){
+
+    @Before
+    fun addBadData() {
+        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+            val path = File(getContext().getExternalFilesDir(null), MainActivity.IMAGE_DIRECTORY)
+            if (!path.exists()) path.mkdir()
+            val path2 = File(getContext().getExternalFilesDir(null), "TEST")
+            val res = path2.mkdir()
+            Log.i("WRITE", "Result of writing star: ${res}")
+            
+            File(path, "star.png").apply {
+                FileOutputStream(this).use {
+                    getTestContext().resources.assets.open("star.png")
+                }
+            }
+        }
+    }
 
     @Test
     fun deleteCrosswordAfterSolveInstrumentedTest() {
