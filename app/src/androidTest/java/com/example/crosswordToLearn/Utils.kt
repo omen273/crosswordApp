1388893b52f7252de5912c498f0255deeb162837
omen273 <38621722+omen273@users.noreply.github.com>
2021-01-25 09:21:54 +0100

    76-try-to-fix

--- app/src/androidTest/java/com/example/crosswordToLearn/Utils.kt
+++ app/src/androidTest/java/com/example/crosswordToLearn/Utils.kt
@@ -79,6 +79,8 @@ fun generateCrossword(): Crossword {
     chooseFirstTopic()
     onView(isRoot()).perform(waitForView(withId(R.id.ok_play)))
     onView(withId(R.id.ok_play)).perform(ViewActions.click())
+    val start = System.currentTimeMillis()
+    waitForCondition("", { System.currentTimeMillis() - start > 2500 })
     onView(isRoot()).perform(waitForView(withId(R.id.crossword)))
     Espresso.pressBack()
     Espresso.pressBack()
