76726007e81156da1084b9876f67422ae2dbda67
omen273 <38621722+omen273@users.noreply.github.com>
2021-01-25 19:31:35 +0100

    76-try-to-fix

--- app/src/androidTest/java/com/example/crosswordToLearn/Utils.kt
+++ app/src/androidTest/java/com/example/crosswordToLearn/Utils.kt
@@ -2,7 +2,6 @@ package com.example.crosswordToLearn
 
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
@@ -14,7 +13,6 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.RootMatchers
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.espresso.util.HumanReadables
 import androidx.test.espresso.util.TreeIterables
@@ -80,12 +78,12 @@ fun generateCrossword(): Crossword {
     onView(isRoot()).perform(waitForView(withId(R.id.ok_play)))
     onView(withId(R.id.ok_play)).perform(ViewActions.click())
     val start = System.currentTimeMillis()
-    waitForCondition("", { System.currentTimeMillis() - start > 10000 })
+    waitForCondition("", { System.currentTimeMillis() - start > 1000 })
     onView(isRoot()).perform(waitForView(withId(R.id.crossword)))
     Espresso.pressBack()
     Espresso.pressBack()
     val start1 = System.currentTimeMillis()
-    waitForCondition("", { System.currentTimeMillis() - start1 > 10000 })
+    waitForCondition("", { System.currentTimeMillis() - start1 > 1000 })
     return getLastCrossword()
 }
 
@@ -99,13 +97,9 @@ fun getLastCrossword(): Crossword {
         getContext().getExternalFilesDir(null),
         MainActivity.IMAGE_DIRECTORY
     )
-    var last: File? = imagesPath.listFiles()?.iterator()?.next()
-    for (file in imagesPath.listFiles()) {
-        if (last!!.lastModified()!! < file.lastModified()) last = file
-    }
+    val last: File = imagesPath.listFiles()?.maxByOrNull { it.lastModified() } ?: throw RuntimeException("Screen shoot doesn't exist")
     val crosswordName =
-        last?.name?.removeSuffix(MainActivity.IMAGE_FORMAT) + GameActivity.DATA_SUFFIX
-
+        last.name.removeSuffix(MainActivity.IMAGE_FORMAT) + GameActivity.DATA_SUFFIX
     return getContext().openFileInput(crosswordName).use {
         buildCrossword { UClickJsonFormatter().read(this, it) }
     }
