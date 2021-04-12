b28211ee656d68fb633fe5959212ace183817b62
omen273 <38621722+omen273@users.noreply.github.com>
2021-04-12 20:26:07 +0200

    138-add test

---
+++ app/src/androidTest/java/com/omen273/crossLingo/LoadSolvedCrosswordSeveralTimes.kt
@@ -0,0 +1,28 @@
+package com.omen273.crossLingo
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.RootMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import org.junit.Test
+
+class LoadSolvedCrosswordSeveralTimes: SolveCrossword() {
+
+    @Test
+    fun solveCrosswordInstrumentedTest() {
+        crossword = generateCrossword()
+        loadFirstCrossword()
+        solve()
+        Espresso.onView(ViewMatchers.withText(R.string.another_crossword))
+            .inRoot(RootMatchers.isDialog())
+            .perform(ViewActions.click())
+        for(i in 0 .. 2) {
+            Espresso.onView(ViewMatchers.isRoot())
+                .perform(waitForView(ViewMatchers.withId(R.id.tableLayout)))
+            Espresso.onView(getItemFromCrosswordList(0, 1)).perform(ViewActions.click())
+            Espresso.onView(ViewMatchers.withText(R.string.another_crossword))
+                .inRoot(RootMatchers.isDialog())
+                .perform(ViewActions.click())
+        }
+    }
+}
