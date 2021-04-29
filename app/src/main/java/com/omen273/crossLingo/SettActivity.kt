a1379cfad59f2b7993f58670089432c9557a83f7
omen273 <38621722+omen273@users.noreply.github.com>
2021-04-29 15:43:50 +0200

    168 add settings and ability to choose level

--- app/src/main/java/com/omen273/crossLingo/SettActivity.kt
+++ app/src/main/java/com/omen273/crossLingo/SettActivity.kt
@@ -2,10 +2,18 @@ package com.omen273.crossLingo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar_game.*
+import kotlinx.android.synthetic.main.toolbar_sett.*
 
 class SettActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sett)
+        setSupportActionBar(sett_toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        sett_toolbar.setNavigationOnClickListener{
+            onBackPressed()
+        }
     }
 }
