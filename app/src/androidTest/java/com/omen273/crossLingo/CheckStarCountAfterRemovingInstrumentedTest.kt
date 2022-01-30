55f517462ab4949d0c4c86467fabe7f758ef2d57
greenEkatherine <sokolik-katya@mail.ru>
2022-01-30 11:35:21 +0100

    301: check more hints in test

--- app/src/androidTest/java/com/omen273/crossLingo/CheckStarCountAfterRemovingInstrumentedTest.kt
+++ app/src/androidTest/java/com/omen273/crossLingo/CheckStarCountAfterRemovingInstrumentedTest.kt
@@ -10,15 +10,15 @@ class CheckStarCountAfterRemovingInstrumentedTest: SolveCrossword() {
 
     @Test
     fun checkStarCountAfterRemovingInstrumentedTest() {
-        val START_STAR_NAMBER = 50
-        waitForCondition("Stars number checking", {START_STAR_NAMBER  == readStarsFromConfig()})
+        val START_STAR_NUMBER = 50
+        waitForCondition("Stars number checking", {START_STAR_NUMBER  == readStarsFromConfig()})
         crossword = generateCrossword()!!
         loadFirstCrossword()
         solve()
         Espresso.onView(ViewMatchers.withText(R.string.remove))
             .inRoot(RootMatchers.isDialog())
             .perform(ViewActions.click())
-        val number = START_STAR_NAMBER + GameActivity.BONUS_ON_SOLVE
+        val number = START_STAR_NUMBER + GameActivity.BONUS_ON_SOLVE
         waitForCondition("Stars number checking", {number == readStarsFromConfig()})
     }
 }
